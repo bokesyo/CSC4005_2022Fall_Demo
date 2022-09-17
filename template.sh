61e6e09e48d1eb654af78ade8dd70443fcf22304
lyzx2001 <rep_laureline@hotmail.com>
2022-09-17 15:41:54 +0800

    update template.sh

---
+++ template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --job-name=parallel_job_test # Job name
+#SBATCH --nodes=1                    # Run all processes on a single node	
+#SBATCH --ntasks=4                   # number of processes = 4
+#SBATCH --cpus-per-task=4            # Number of CPU cores per process
+#SBATCH --mem=600mb                  # Total memory limit
+#SBATCH --time=00:05:00              # Time limit hrs:min:sec
+
+mpirun -np 4 ./xxxxx
