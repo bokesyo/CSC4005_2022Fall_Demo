98e5142d2a70412dcaac2a75c71cb36c79ef1954
Boke Syo <63912729+bokesyo@users.noreply.github.com>
2022-10-24 21:22:37 +0800

    Update pthread.cpp

--- project2_template/pthread.cpp
+++ project2_template/pthread.cpp
@@ -16,7 +16,7 @@ typedef struct {
 
 void* worker(void* args) {
     //TODO: procedure in each threads
-    // the code following is not a necessary, you can replace it.
+    // the code following is not necessary, you can replace it.
     
     // Args* my_arg = (Args*) args;
     // int a = my_arg->a;
@@ -53,32 +53,32 @@ int main(int argc, char *argv[]) {
 	glutDisplayFunc(plot);
     #endif
 
-	/* computation part begin */
+    /* computation part begin */
     t1 = std::chrono::high_resolution_clock::now();
 
     initData();
 
     //TODO: assign jobs
-    pthread_t thds[n_thd]; // thread pool
-    Args args[n_thd]; // arguments for all threads
-    for (int thd = 0; thd < n_thd; thd++){
-        args[thd].a = thd;
-        args[thd].b = n_thd;
-    }
-    for (int thd = 0; thd < n_thd; thd++) pthread_create(&thds[thd], NULL, worker, &args[thd]);
-    for (int thd = 0; thd < n_thd; thd++) pthread_join(thds[thd], NULL);
+    //     pthread_t thds[n_thd]; // thread pool
+    //     Args args[n_thd]; // arguments for all threads
+    //     for (int thd = 0; thd < n_thd; thd++){
+    //         args[thd].a = thd;
+    //         args[thd].b = n_thd;
+    //     }
+    //     for (int thd = 0; thd < n_thd; thd++) pthread_create(&thds[thd], NULL, worker, &args[thd]);
+    //     for (int thd = 0; thd < n_thd; thd++) pthread_join(thds[thd], NULL);
     //TODO END
 
     t2 = std::chrono::high_resolution_clock::now();  
-	time_span = t2 - t1;
-	/* computation part end */
+    time_span = t2 - t1;
+    /* computation part end */
 
     printf("Student ID: 119010001\n"); // replace it with your student id
-	printf("Name: Your Name\n"); // replace it with your name
-	printf("Assignment 2 Pthread\n");
-	printf("Run Time: %f seconds\n", time_span.count());
-	printf("Problem Size: %d * %d, %d\n", X_RESN, Y_RESN, max_iteration);
-	printf("Thread Number: %d\n", n_thd);
+    printf("Name: Your Name\n"); // replace it with your name
+    printf("Assignment 2 Pthread\n");
+    printf("Run Time: %f seconds\n", time_span.count());
+    printf("Problem Size: %d * %d, %d\n", X_RESN, Y_RESN, max_iteration);
+    printf("Thread Number: %d\n", n_thd);
 
     #ifdef GUI
 	glutMainLoop();
