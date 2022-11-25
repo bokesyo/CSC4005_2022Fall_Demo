1319be5e1ac2dcd7e63873df8035ac225bec0576
22 <demo@cuhk.edu.cn>
2022-11-25 01:21:22 -0500

    remove simple demo

--- openmp_demo/openmp-thread-num.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <omp.h>
-#include <vector>
-
-int main() {
-#pragma omp parallel
-    { std::cout << "Thread number: " << omp_get_thread_num() << std::endl; }
-    return 0;
-}
--- template.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=your_job_name # Job name
-#SBATCH --nodes=1                    # Run all processes on a single node	
-#SBATCH --ntasks=20                   # number of processes = 20
-#SBATCH --cpus-per-task=1            # Number of CPU cores allocated to each process
-#SBATCH --partition=Project            # Partition name: Project or Debug (Debug is default)
-
-cd /nfsmnt/119010355/CSC4005_2022Fall_Demo/project1_template/
-mpirun -np 4 ./psort 10000 ./test_data/10000a.in
